868d70f896fb1843846de84ee49dd8794b227a2d
Xavier Fischer <xavier@fischer.ninja>
2020-03-14 08:40:46 -0700

    upload and update

--- DEM.Net.Extension.SketchFab/Export/SketchFabExporter.cs
+++ DEM.Net.Extension.SketchFab/Export/SketchFabExporter.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -18,6 +20,11 @@ namespace DEM.Net.Extension.SketchFab.Export
         private readonly HttpClient _httpClient;
         private readonly JsonSerializerOptions _jsonSerializerOptions;
         private const string SketchFabApi = "https://api.sketchfab.com/v3";
+        private readonly DefaultContractResolver contractResolver = new DefaultContractResolver
+        {
+            NamingStrategy = new CamelCaseNamingStrategy()
+        };
+
 
         /*
         # Uploading a model to Sketchfab is a two step process
@@ -43,13 +50,32 @@ namespace DEM.Net.Extension.SketchFab.Export
             this._httpClient = new HttpClient();
             this._jsonSerializerOptions = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, IgnoreNullValues = true };
             _jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, false));
+
         }
 
+
+        public async Task<string> UploadModel(UploadModelRequest request)
+        {
+            try
+            {
+                string uuid = await UploadFile(request);
+
+                await UpdateModel(uuid, request);
+
+                return request.ModelId;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"SketchFab upload error: {ex.Message}");
+                throw;
+            }
+
+        }
         public async Task<string> UploadFile(UploadModelRequest request)
         {
             try
             {
-                _logger.LogInformation($"Uploading a text file [{request.FilePath}].");
+                _logger.LogInformation($"Uploading model [{request.FilePath}].");
                 if (string.IsNullOrWhiteSpace(request.FilePath))
                 {
                     throw new ArgumentNullException(nameof(request.FilePath));
@@ -75,16 +101,14 @@ namespace DEM.Net.Extension.SketchFab.Export
                 form.Add(new StringContent("tags"), string.Join(Environment.NewLine, request.Tags));
                 form.Add(new StringContent("categories"), string.Join(Environment.NewLine, request.Categories));
 
-                // Seems not working yet. TODO test with PATCH after
-                //var optionsJson = JsonSerializer.Serialize(request.Options, _jsonSerializerOptions);
-                //form.Add(new StringContent("options"), optionsJson);
-
                 httpRequestMessage.Content = form;
 
 
                 var response = await _httpClient.SendAsync(httpRequestMessage, HttpCompletionOption.ResponseContentRead);
+                _logger.LogInformation($"{nameof(UploadFile)} responded {response.StatusCode}");
                 response.EnsureSuccessStatusCode();
                 string uuid = response.Headers.GetValues("Location").FirstOrDefault();
+                request.ModelId = uuid;
                 _logger.LogInformation("Uploading is complete. Model uuid is " + uuid);
                 return uuid;
             }
@@ -96,6 +120,45 @@ namespace DEM.Net.Extension.SketchFab.Export
 
         }
 
+        public async Task UpdateModel(string modelUuid, UploadModelRequest request)
+        {
+            try
+            {
+                _logger.LogInformation($"Updating model [{request.Name}].");
+                if (string.IsNullOrWhiteSpace(modelUuid))
+                {
+                    throw new ArgumentNullException(nameof(modelUuid));
+                }
+
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Patch, $"{SketchFabApi}/models/{modelUuid}");
+                httpRequestMessage.Headers.Add("Authorization", $"Token {request.Token}");
+
+                using var form = new MultipartFormDataContent("abcdef");
+                form.Headers.ContentType.MediaType = "multipart/form-data";
+                form.Add(new StringContent("name"), ShortenString(request.Name, 48));
+                form.Add(new StringContent("description"), ShortenString(request.Description, 1024));
+                form.Add(new StringContent("private"), request.IsPrivate ? "0" : "1");
+                form.Add(new StringContent("isInspectable"), request.IsInspectable ? "0" : "1");
+                form.Add(new StringContent("isPublished"), request.IsPublished ? "0" : "1");
+                form.Add(new StringContent("license"), request.License);
+                form.Add(new StringContent("tags"), string.Join(Environment.NewLine, request.Tags));
+                form.Add(new StringContent("categories"), string.Join(Environment.NewLine, request.Categories));
+
+                httpRequestMessage.Content = form;
+
+                var response = await _httpClient.SendAsync(httpRequestMessage);
+
+                _logger.LogInformation($"{nameof(UpdateModel)} responded {response.StatusCode}");
+                response.EnsureSuccessStatusCode();
+               
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"SketchFab update error: {ex.Message}");
+                throw;
+            }
+
+        }
         private string ShortenString(string str, int length)
         {
             return str.Substring(0, Math.Min(str.Length, length));
--- DEM.Net.Extension.SketchFab/Export/UploadModelRequest.cs
+++ DEM.Net.Extension.SketchFab/Export/UploadModelRequest.cs
@@ -1,26 +1,40 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace DEM.Net.Extension.SketchFab.Export
 {
     public class UploadModelRequest
     {
-        public string Token { get; set; }
-        public string FilePath { get; set; }
+
+        /// <summary>
+        /// SketchFab uuid
+        /// </summary>
+        [JsonProperty("uid")]
+        public string ModelId { get; set; }
+
+
+        [JsonProperty("name")]
         public string Name { get; set; }
+
         /// <summary>
         /// Sets private [pro only]
         /// </summary>
+
+        [JsonProperty("private")]
         public bool IsPrivate { get; set; }
 
         /// <summary>
         /// Enables 2D view in model inspector. All downloadable models must have isInspectable enabled.
         /// </summary>
+        [JsonProperty("isInspectable")]
         public bool IsInspectable { get; set; }
 
         /// <summary>
         /// Sets a password [pro only]
         /// </summary>
+        [JsonProperty("password")]
         public string Password { get; set; }
+
 
 
         /// <summary>
@@ -29,18 +43,42 @@ namespace DEM.Net.Extension.SketchFab.Export
         /// 
         /// 
         /// </summary>
+        [JsonProperty("license")]
         public string License { get; set; } = "by-nc-sa";
 
+
+        [JsonIgnore]
         public List<string> Tags { get; set; } = new List<string> { "elevationapi" };
+
+        [JsonProperty("tags")]
+        public string TagsJson => string.Join("\n", Tags);
+
+        [JsonIgnore]
         public List<string> Categories { get; set; } = new List<string> { "cultural-heritage-history", "places-travel" };
+
+        [JsonProperty("categories")]
+        public string CategoriesJson => string.Join("\n", Categories);
 
         /// <summary>
         /// Sets published after it is processed
         /// </summary>
+        [JsonProperty("isPublished")]
         public bool IsPublished { get; set; }
 
+
+
+        [JsonProperty("description")]
         public string Description { get; set; }
 
+        [JsonProperty("options")]
         public ModelOptions Options { get; set; } = new ModelOptions();
+
+        [JsonIgnore]
+        public string Token { get; set; }
+
+        [JsonIgnore]
+        public string FilePath { get; set; }
+
+
     }
 }
